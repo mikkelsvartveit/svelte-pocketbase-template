277112e42d01f0967b53e0b0233ac811ea271484
Mikkel Svartveit <mikkel.svartveit@gmail.com>
2025-08-29 21:02:12 +0200

    Update handle hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,10 +3,29 @@ import PocketBase from "pocketbase";
 import type { TypedPocketBase } from "$lib/pocketbase-typegen";
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const serverPb = new PocketBase() as TypedPocketBase;
-  serverPb.authStore.loadFromCookie(event.cookies.get.toString());
+  event.locals.serverPb = new PocketBase() as TypedPocketBase;
 
-  event.locals.serverPb = serverPb;
+  event.locals.serverPb.authStore.loadFromCookie(
+    event.request.headers.get("cookie") || "",
+  );
 
-  return resolve(event);
+  try {
+    // get an up-to-date auth store state by verifying and refreshing the loaded auth model (if any)
+    if (!event.locals.serverPb.authStore.isValid) {
+      await event.locals.serverPb.collection("users").authRefresh();
+    }
+  } catch {
+    // clear the auth store on failed refresh
+    event.locals.serverPb.authStore.clear();
+  }
+
+  const response = await resolve(event);
+
+  // send back the default 'pb_auth' cookie to the client with the latest store state
+  response.headers.append(
+    "set-cookie",
+    event.locals.serverPb.authStore.exportToCookie(),
+  );
+
+  return response;
 };
